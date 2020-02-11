f9768695355040995e7d129710d2dc2ff67c1b46
Anugrah Jaya <anugrahjaya23@gmail.com>
2020-02-11 20:26:34 +0700

    edit seed untuk id_program_studi

--- database/seeds/Program_Studi.php
+++ database/seeds/Program_Studi.php
@@ -11,29 +11,35 @@ class Program_Studi extends Seeder
      * @return void
      */
 
-     private $id_fakultas = array(1,2,3,4,5,6,7);
-     private $count = array(3,1,3,2,1,3,3);
-     private $program_studi = array(array("Ekonomi Pembangunan","Manajemen","Akuntansi"),
-                            array("Ilmu Hukum"),
-                            array("Ilmu Administrasi Publik","Ilmu Administrasi Bisnis","Ilmu Hubungan Internasional"),
-                            array("Teknik Sipil","Arsitektur"),
-                            array("Ilmu Filsafat"),
-                            array("Teknik Industri","Teknik Kimia","Teknik Elektro"),
-                            array("Matematika","Fisika","Teknik Informatika"));
-    
-    public function run(){
-        for($i = 1; $i<=7; $i++){
-            $id_f = $this->id_fakultas[$i-1]*100;
-            if($this->id_fakultas[$i-1]==2){
-                $id_program_studi = $id_f;
-            }else{
-                $id_program_studi = $id_f+($i*10);
-            }
+    private $id_fakultas = array(1, 2, 3, 4, 5, 6, 7);
+    private $count = array(3, 1, 3, 2, 1, 3, 3);
+    private $program_studi = array(
+        array("Ekonomi Pembangunan", "Manajemen", "Akuntansi"),
+        array("Ilmu Hukum"),
+        array("Ilmu Administrasi Publik", "Ilmu Administrasi Bisnis", "Ilmu Hubungan Internasional"),
+        array("Teknik Sipil", "Arsitektur"),
+        array("Ilmu Filsafat"),
+        array("Teknik Industri", "Teknik Kimia", "Teknik Elektro"),
+        array("Matematika", "Fisika", "Teknik Informatika")
+    );
 
-            for($j = 1; $j<= $this->count[$i-1];$j++){
+    public function run()
+    {
+        for ($i = 1; $i <= 7; $i++) {
+            
+            $id_f = $this->id_fakultas[$i - 1] * 100;
+
+            // untuk id program studi/kode
+            for ($j = 1; $j <= $this->count[$i - 1]; $j++) {
+                // jika fakultas hukum
+                if ($this->id_fakultas[$i - 1] == 2) {
+                    $id_program_studi = $id_f;
+                } else {
+                    $id_program_studi = $id_f + ($j * 10);
+                }
                 DB::table("program_studi")->insert([
                     "id_program_studi" => $id_program_studi,
-                    "nama_program_studi" => $this->program_studi[$this->program_studi[$j-1]],
+                    "nama_program_studi" => $this->program_studi[$this->program_studi[$j - 1]],
                     "id_fakultas" => $id_f
                 ]);
             }
